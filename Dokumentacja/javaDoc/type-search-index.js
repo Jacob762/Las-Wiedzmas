65734a5a50fd1ead338b23c7457af8a33152f3cc
matematykmiero2 <105746899+matematykmiero2@users.noreply.github.com>
2022-06-03 21:46:47 +0200

    Dokumentacja
    Diagram klas
    Diagram obiektów
    Opis w języku naturalnym
    JavaDoc

---
+++ Dokumentacja/javaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lasW","l":"Epoka"},{"p":"lasW","l":"Epoka_Implementacja"},{"p":"lasW","l":"Funkcje"},{"p":"lasW","l":"GUI"},{"p":"lasW","l":"LasWiedzmas"}];updateSearchResults();
